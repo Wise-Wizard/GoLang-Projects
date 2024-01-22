8bb5e93ed370204fd9ca33775832d6de4adde667
Wise-Wizard <saransh.shankar@gmail.com>
2024-01-23 00:16:40 +0530

    Creating Product-API

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Wise-Wizard/GoLang-Projects
+
+go 1.21.6
--- product-api/main.go
+++ product-api/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", serveHome)
+	http.ListenAndServe(":9090", nil)
+	fmt.Println("Server is listening on :9090...")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, this is the home page!")
+}
