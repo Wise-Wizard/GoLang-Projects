0c6f26bf8f920fade2191068f6fb2deaee2d9990
Wise-Wizard <saransh.shankar@gmail.com>
2024-01-24 00:33:43 +0530

    Added Tests
    Signed-off-by: Wise-Wizard <saransh.shankar@gmail.com>

--- go.mod
+++ go.mod
@@ -7,8 +7,11 @@ require (
 	github.com/gorilla/mux v1.8.1
 )
 
+require github.com/felixge/httpsnoop v1.0.3 // indirect
+
 require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/gorilla/handlers v1.5.2
 	github.com/leodido/go-urn v1.2.4 // indirect
 )
--- product-api/main.go
+++ product-api/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/Wise-Wizard/GoLang-Projects/product-api/handlers"
+	gohandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -43,10 +44,12 @@ func main() {
 
 	//sm.Handle("/products", ph)
 
+	//CORS
+	ch := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"*"}))
 	// create a new server
 	s := http.Server{
 		Addr:         ":9090",           // configure the bind address
-		Handler:      sm,                // set the default handler
+		Handler:      ch(sm),            // set the default handler
 		ErrorLog:     l,                 // set the logger for the server
 		ReadTimeout:  5 * time.Second,   // max time to read request from the client
 		WriteTimeout: 10 * time.Second,  // max time to write response to the client
