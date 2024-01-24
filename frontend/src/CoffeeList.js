d63bd08a565de83632c9cfc3b313d62c786143a3
Wise-Wizard <saransh.shankar@gmail.com>
2024-01-24 18:07:33 +0530

    MultPart Form Reader

--- frontend/src/CoffeeList.js
+++ frontend/src/CoffeeList.js
@@ -1,63 +1,70 @@
-import React from "react";
-import Table from "react-bootstrap/Table";
-import axios from "axios";
+import React from 'react';
+import Table from 'react-bootstrap/Table'
+import axios from 'axios';
 
 class CoffeeList extends React.Component {
-  readData() {
-    const self = this;
-    axios
-      .get("http://localhost:9090")
-      .then(function (response) {
-        console.log(response.data);
 
-        self.setState({ products: response.data });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
+    readData() {
+        const self = this;
+        axios.get('http://localhost:9090').then(function(response) {
+            console.log(response.data);
 
-  getProducts() {
-    let table = [];
-
-    for (let i = 0; i < this.state.products.length; i++) {
-      table.push(
-        <tr key={i}>
-          <td>{this.state.products[i].name}</td>
-          <td>{this.state.products[i].price}</td>
-          <td>{this.state.products[i].sku}</td>
-        </tr>
-      );
+            self.setState({products: response.data});
+        }).catch(function (error){
+            console.log(error);
+        });
     }
 
-    return table;
-  }
+    getProducts() {
+        let table = []
 
-  constructor(props) {
-    super(props);
-    this.readData();
-    this.state = { products: [] };
+        for (let i=0; i < this.state.products.length; i++) {
 
-    this.readData = this.readData.bind(this);
-  }
-
-  render() {
-    return (
-      <div>
-        <h1 style={{ marginBottom: "40px" }}>Menu</h1>
-        <Table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Price</th>
-              <th>SKU</th>
+            table.push(
+            <tr key={i}>
+                <td>{this.state.products[i].name}</td>
+                <td>{this.state.products[i].price}</td>
+                <td>{this.state.products[i].sku}</td>
             </tr>
-          </thead>
-          <tbody>{this.getProducts()}</tbody>
-        </Table>
-      </div>
-    );
-  }
+            );
+        }
+
+        return table
+    }
+
+    constructor(props) {
+        super(props);
+        this.readData();
+        this.state = {products: []};
+    
+        this.readData = this.readData.bind(this);
+    }
+
+    render() {
+      return (
+        <div>
+            <h1 style={{marginBottom: "40px"}}>Menu</h1>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>
+                            Name
+                        </th>
+                        <th>
+                            Price
+                        </th>
+                        <th>
+                            SKU
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.getProducts()}
+                </tbody>
+            </Table>
+        </div>
+      ) 
+    }
 }
 
 export default CoffeeList;
